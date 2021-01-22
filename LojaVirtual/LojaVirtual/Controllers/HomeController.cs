e68108cd7050e88f0e1ec8e4c36e107631f660bf
BRENO MORAIS <62041808+BREN0-MORAIS@users.noreply.github.com>
2021-01-21 20:19:54 -0400

    Layout implementado
    Foi implementado o Layout da pagina

--- LojaVirtual/LojaVirtual/Controllers/HomeController.cs
+++ LojaVirtual/LojaVirtual/Controllers/HomeController.cs
@@ -1,37 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using LojaVirtual.Models;
 
 namespace LojaVirtual.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Contato()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Login()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
+        }  
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
         }
     }
 }
