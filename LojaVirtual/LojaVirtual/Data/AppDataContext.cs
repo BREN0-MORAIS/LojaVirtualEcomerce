b3a2872e70954f6ae9d3b8580cee56629c41829e
BRENO MORAIS <62041808+BREN0-MORAIS@users.noreply.github.com>
2021-01-23 13:22:55 -0400

    Contexto
    Contexto e criação de Model de Cliente criados.

---
+++ LojaVirtual/LojaVirtual/Data/AppDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Data
+{
+    public class AppDataContext : DbContext
+    {
+        //passa o contexto recebido para o construtor base da classe DbContext
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
